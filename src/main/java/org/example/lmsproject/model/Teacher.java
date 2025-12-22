a19533c85d9b026c277014310bcbd7afbe6eef33
Sidorov Aleksandr <alexander.sidorov@bk.ru>
2025-12-23 00:06:05 +0300

    Add Constructor To Teacher

--- src/main/java/org/example/lmsproject/model/Teacher.java
+++ src/main/java/org/example/lmsproject/model/Teacher.java
@@ -1,10 +1,7 @@
 package org.example.lmsproject.model;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
@@ -13,6 +10,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Teacher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
